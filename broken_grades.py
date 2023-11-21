50642d4cc4724a1d7310baa46d9e08bba526233e
Kayy777 <151622717+Kayy777@users.noreply.github.com>
2023-11-21 11:55:01 +0000

    Add files via upload

--- broken_grades.py
+++ broken_grades.py
@@ -21,9 +21,9 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: ")
+exam_two = int(input("Input exam grade two: "))
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: "))
 
 grades = [exam_one+exam_two+exam_three]
 sum = 0
@@ -37,10 +37,10 @@ if avg >= 90:
 elif avg >= 80 and avg < 90:
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C"
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else:
     letter_grade = "F"
 
 for grade in grades:
@@ -51,6 +51,6 @@ for grade in grades:
     print("Grade: " + letter_grade)
 
 if letter-grade is "F":
-    print "Student is failing."
+    print("Student is failing.")
 else:
-    print "Student is passing."
+    print("Student is passing.")
