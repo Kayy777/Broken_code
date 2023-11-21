84647592dbafb2a951bd65b0d6e903d0909bad05
Kayy777 <151622717+Kayy777@users.noreply.github.com>
2023-11-21 11:29:31 +0000

    Add files via upload

--- broken_travel.py
+++ broken_travel.py
@@ -12,5 +12,5 @@ if year <= 1900:
     print ("Woah, that's the past!")
 elif year > 1900 == year < 2020:
     print ("That's totally the present!")
-elif:
+else:
     print ("Far out, that's the future!!")
